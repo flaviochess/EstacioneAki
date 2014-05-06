872ecce507645d9f58d6f3894f112068bfba208f
Willian Teophilo <wilcido@gmail.com>
2014-05-05 22:00:17 -0300

    Incluindo login

--- src/main/java/br/usp/sistemasdeinformacao/estacioneaki/dao/EstacionamentoDAO.java
+++ src/main/java/br/usp/sistemasdeinformacao/estacioneaki/dao/EstacionamentoDAO.java
@@ -8,22 +8,37 @@ package br.usp.sistemasdeinformacao.estacioneaki.dao;
 
 import br.usp.sistemasdeinformacao.estacioneaki.entity.Estacionamento;
 import br.usp.sistemasdeinformacao.estacioneaki.sistema.GenericDAO;
+
+import org.hibernate.Criteria;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 /**
- *
+ * 
  * @author flavio
  */
 @Repository
-public class EstacionamentoDAO extends GenericDAO<Estacionamento, Integer>{
-    
-    @Autowired
-    private SessionFactory sessionFactory;
+public class EstacionamentoDAO extends GenericDAO<Estacionamento, Integer> {
 
-    @Override
-    public SessionFactory getSessionFactory() {
-        return this.sessionFactory;
-    }
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	@Override
+	public SessionFactory getSessionFactory() {
+		return this.sessionFactory;
+	}
+
+	public Estacionamento buscaEstacionamentoPorEmailESenha(String email,
+			String senha) {
+		if (!email.isEmpty() && !senha.isEmpty()) {
+			Criteria criteria = sessionFactory.getCurrentSession()
+					.createCriteria(Estacionamento.class);
+			criteria.add(Restrictions.ilike("email", email));
+			criteria.add(Restrictions.ilike("senha", senha));
+			return (Estacionamento) criteria.uniqueResult();
+		}
+		return null;
+	}
 }
